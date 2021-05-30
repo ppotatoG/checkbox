b5fc2cfa86e88f873c2439443789c7545bc6f1c3
ppotatoG <thgus2270@gmail.com>
2021-05-30 20:10:18 +0900

    eventListner로 단일체크

--- js/event.js
+++ js/event.js
@@ -1,7 +1,8 @@
-const checkboxes = document.querySelectorAll('input[name=chk01_El]');
+const checkboxes = document.querySelectorAll('input[name=chk]');
 
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener('click', function(){
+    // 여기가 부모의 자식들로 
     for(chk of checkboxes){
       chk.checked = false
     }
