71b3cb7e0907b62e83e2b0c636a7870ae5b38eca
ppotatoG <thgus2270@gmail.com>
2021-06-22 13:43:34 +0900

    모두체크 추가

--- js/event.js
+++ js/event.js
@@ -2,13 +2,22 @@ const checkboxes = document.querySelectorAll('input');
 
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener('click', function(){
-    const thisName = this.name
-    const Siblings = document.querySelectorAll('input[name='+thisName+']')
+    const thisName = this.name;
+    const Siblings = document.querySelectorAll('input[name='+thisName+']');
+
     for(Sibling of Siblings){
-      Sibling.checked = false
-      Sibling.nextElementSibling.style.color = '#000'
+      Sibling.checked = false;
+      Sibling.nextElementSibling.style.color = '#000';
     }
-    this.checked = true
-    this.nextElementSibling.style.color = 'red'
+    
+    this.checked = true;
+    this.nextElementSibling.style.color = 'red';
+
+    if(this.classList == 'all') {
+      for(Sibling of Siblings){
+        Sibling.checked = true;
+        Sibling.nextElementSibling.style.color = 'red';
+      }
+    }
   })
 })
